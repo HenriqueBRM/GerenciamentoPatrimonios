22a9e72e9340baf91113bb08823bda72fccbe2f4
HENRIQUE BOTELHO REIS MARQUES <23971603823@info.local>
2026-05-26 11:00:42 -0300

    :construction: finishing login page

---
+++ Frontend/gestaopatrimonios/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { Montserrat } from "next/font/google";
+import type { AppProps } from "next/app";
+
+const montserrat = Montserrat({
+  variable: "--font-padrao",
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={montserrat.variable}>
+      <Component {...pageProps} />;
+    </main>
+  )
+}
